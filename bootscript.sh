315fcec87b26bfb07e65d9f648ceab9fb5741610
aidanlw17 <aidan.lw17@gmail.com>
2019-02-24 20:50:57 -0500

    initial

---
+++ bootscript.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+(
+dmesg -D
+echo Scanning for wifi networks:
+ifup wlan0
+wpa_cli scan
+echo -e "\nStrongest networks found:"
+wpa_cli scan_res | sort -grk 3 | head | awk -F '\t' '{print $NF}' | uniq
+set -e
+echo -e /"\nWARNING: this script will back up and remove all of your current wifi configs."
+read -p "Press Ctrl-C to cancel, or press Enter to continue:" -r
+echo -e "\nNOTE: Spaces in your network name or password are ok. Do not add quotes."
+read -p "Enter your network name: " -r
+SSID=$REPLY
+read -p "Enter your network password: " -r
+PSK=$REPLY
+cd /etc/network
+cp interfaces interfaces.$(date +%s).bak
+echo -e "auto lo\niface lo inet loopback\n\nauto usb0\niface usb0 inet static\n  address 10.11.12.13\n  netmask 255.255.255.0\n\nauto wlan0\niface wlan0 inet dhcp\n  wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf" > interfaces
+echo -e "\n/etc/network/interfaces:\n"
+cat interfaces
+cd /etc/wpa_supplicant/
+cp wpa_supplicant.conf wpa_supplicant.conf.$(date +%s).bak
+echo -e "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\nnetwork={\n  ssid=\"$SSID\"\n  psk=\"$PSK\"\n}" > wpa_supplicant.conf
+echo -e "\n/etc/wpa_supplicant/wpa_supplicant.conf:\n"
+cat wpa_supplicant.conf
+echo -e "\nAttempting to bring up wlan0:\n"
+ifdown wlan0; ifup wlan0
+sleep 10
+echo -ne "\nWifi SSID: "; iwgetid -r
+sleep 5
+#curl https://raw.githubusercontent.com/openaps/oref0/master/bin/openaps-install.sh > /tmp/openaps-install.sh
+#-------------equivalent to curl--------------
+set -e
+
+read -p "Enter your rig's new hostname (this will be your rig's "name" in the future, so make sure to write it down): " -r
+myrighostname=$REPLY
+echo $myrighostname > /etc/hostname
+sed -r -i"" "s/localhost( jubilinux)?$/localhost $myrighostname/" /etc/hosts
+sed -r -i"" "s/127.0.1.1.*$/127.0.1.1       $myrighostname/" /etc/hosts
+
+# if passwords are old, force them to be changed at next login
+passwd -S edison 2>/dev/null | grep 20[01][0-6] && passwd -e root
+# automatically expire edison account if its password is not changed in 3 days
+passwd -S edison 2>/dev/null | grep 20[01][0-6] && passwd -e edison -i 3
+
+if [ -e /run/sshwarn ] ; then
+    echo Please select a secure password for ssh logins to your rig:
+    echo 'For the "root" account:'
+    passwd root
+    echo 'And for the "pi" account (same password is fine):'
+    passwd pi
+fi
+
+grep "PermitRootLogin yes" /etc/ssh/sshd_config || echo "PermitRootLogin yes" > /etc/ssh/sshd_config
+
+# set timezone
+dpkg-reconfigure tzdata
+
+#dpkg -P nodejs nodejs-dev
+# TODO: remove the `-o Acquire::ForceIPv4=true` once Debian's mirrors work reliably over IPv6
+apt-get -o Acquire::ForceIPv4=true update && apt-get -o Acquire::ForceIPv4=true -y dist-upgrade && apt-get -o Acquire::ForceIPv4=true -y autoremove
+apt-get -o Acquire::ForceIPv4=true update && apt-get -o Acquire::ForceIPv4=true install -y sudo strace tcpdump screen acpid vim python-pip locate ntpdate
+#check if edison user exists before trying to add it to groups
+
+if  getent passwd edison > /dev/null; then
+  echo "Adding edison to sudo users"
+  adduser edison sudo
+  echo "Adding edison to dialout users"
+  adduser edison dialout
+ # else
+  # echo "User edison does not exist. Apparently, you are runnning a non-edison setup."
+fi
+
+sed -i "s/daily/hourly/g" /etc/logrotate.conf
+sed -i "s/#compress/compress/g" /etc/logrotate.conf
+
+#curl -s https://raw.githubusercontent.com/openaps/oref0/master/bin/openaps-packages.sh | bash -
+#----------Equivalent to curl from TODO down------------
+
+# TODO: remove the `-o Acquire::ForceIPv4=true` once Debian's mirrors work reliably over IPv6
+apt-get -o Acquire::ForceIPv4=true install -y sudo
+#My input
+sudo apt-get remove npm
+sudo apt-get remove nodejs-legacy
+sudo apt-get remove nodejs
+sudo rm /usr/bin/node
+sudo apt-get install curl software-properties-common
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+sudo apt-get install nodejs
+
+#End my input
+sudo apt-get -o Acquire::ForceIPv4=true update && sudo apt-get -o Acquire::ForceIPv4=true -y upgrade
+sudo apt-get -o Acquire::ForceIPv4=true install -y git python python-dev software-properties-common python-numpy python-pip watchdog strace tcpdump screen acpid vim locate jq lm-sensors && \
+#if getent passwd edison > /dev/null; then sudo apt-get -o Acquire::ForceIPv4=true install -y nodejs-legacy; fi && \
+sudo pip install -U openaps && \
+sudo pip install -U openaps-contrib && \
+sudo openaps-install-udev-rules && \
+sudo activate-global-python-argcomplete && \
+sudo npm install -g json oref0 && \
+echo openaps installed && \
+openaps --version
+
+#After second curl
+mkdir -p ~/src; cd ~/src && git clone git://github.com/openaps/oref0.git || (cd oref0 && git checkout master && git pull)
+echo "Press Enter to run oref0-setup with the current release (master branch) of oref0,"
+read -p "or press ctrl-c to cancel. " -r
+cd && ~/src/oref0/bin/oref0-setup.sh
+#After first curl
+bash /tmp/openaps-install.sh
+)
